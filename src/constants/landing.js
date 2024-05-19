38f9cdcd2c57060262bb3e7dd129cd0e336ac82d
Pooriadevx <pooria.b1378@gmail.com>
2024-05-19 11:49:05 +0330

    Created required constants for landing page

---
+++ src/constants/landing.js
@@ -0,0 +1,64 @@
+import allSizes from "../assets/allSizes.svg";
+import code from "../assets/code.svg";
+import time from "../assets/time.svg";
+import user1 from "../assets/User.svg";
+import user2 from "../assets/User2.svg";
+import user3 from "../assets/User3.svg";
+
+export const FibersBenefits = [
+  {
+    title: "Build In Minutes",
+    description:
+      "With a selection of premade templates, you can build out a portfolio in less than 10 minutes.",
+    img: time,
+  },
+  {
+    title: "Add Custom",
+    description:
+      "Cutomize your personal portfolio even more with the ability to add your own custom css styles",
+    img: code,
+  },
+  {
+    title: "Responsive",
+    description:
+      "All Fiber templates are fully responsive to ensure the experience is seemless across all devices",
+    img: allSizes,
+  },
+];
+
+export const PersonsComments = [
+  {
+    name: "Sarah Andrew",
+    number: "100k in revenue",
+    img: user1,
+  },
+  {
+    name: "Mathew Higgins",
+    number: "100k in revenue",
+    img: user2,
+  },
+  {
+    name: "Janice Dave",
+    number: "100k in revenue",
+    img: user3,
+  },
+];
+
+export const FooterData = [
+  {
+    title: "Sitemap",
+    list: ["HomePage", "Pricing", "Testimotinals", "Features"],
+  },
+  {
+    title: "Resource",
+    list: ["Support", "Contact", "FAQ"],
+  },
+  {
+    title: "Company",
+    list: ["About", "Customers", "Blog"],
+  },
+  {
+    title: "Portfolios",
+    list: ["Sarah Andrews", "Mathew Higgins", "Janice Dave"],
+  },
+];
